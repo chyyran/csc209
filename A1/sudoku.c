1352a789e503ab2d749562b625395f898a3cee6b
Ronny Chan <ronny6993@gmail.com>
2018-09-22 16:44:06 -0400

    Sudoku

--- A1/sudoku.c
+++ A1/sudoku.c
@@ -2,24 +2,28 @@
 
 int check_regular_sudoku(int **puzzle);
 
-int main() {
-    // You could change these values to hand-test your function. 
+int main()
+{
+    // You could change these values to hand-test your function.
     // This example is invalid. Notice that the very middle inner 3x3
     // box has a pair of 3's and a pair of 4's, no 6 and no 7.
-    int row1[9] = {1, 2, 3, 4, 5, 6, 7, 8 ,9};
-    int row2[9] = {4, 5, 6, 7, 8 ,9, 1, 2, 3};
-    int row3[9] = {7, 8 ,9, 1, 2, 3, 4, 5, 6};
-    int row4[9] = {9, 1, 2, 3, 4, 5, 6, 7, 8};
-    int row5[9] = {5, 6, 7, 8 ,9, 1, 2, 3, 4};
-    int row6[9] = {8 ,9, 1, 2, 3, 4, 5, 6, 7};
-    int row7[9] = {3, 4, 5, 6, 7, 8 ,9, 1, 2};
-    int row8[9] = {6, 7, 8 ,9, 1, 2, 3, 4, 5};
-    int row9[9] = {2, 3, 4, 5, 6, 7, 8, 9, 1};
+    int row1[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int row2[9] = {7, 8, 9, 2, 2, 3, 4, 5, 6};
+    int row3[9] = {4, 5, 6, 7, 8, 9, 1, 2, 3};
+    int row4[9] = {3, 1, 2, 8, 4, 5, 9, 6, 7};
+    int row5[9] = {6, 9, 7, 3, 1, 2, 8, 4, 5};
+    int row6[9] = {8, 4, 5, 6, 9, 7, 3, 1, 2};
+    int row7[9] = {2, 3, 1, 5, 7, 4, 6, 9, 8};
+    int row8[9] = {9, 6, 8, 2, 3, 1, 5, 7, 4};
+    int row9[9] = {5, 7, 4, 9, 6, 8, 2, 3, 1};
 
     int *puzzle[9] = {row1, row2, row3, row4, row5, row6, row7, row8, row9};
-    if (check_regular_sudoku(puzzle)) {
+    if (check_regular_sudoku(puzzle))
+    {
         printf("Invalid Sudoku\n");
-    } else {
+    }
+    else
+    {
         printf("Valid Sudoku\n");
     }
 
--- A1/sudoku_helpers.c
+++ A1/sudoku_helpers.c
@@ -5,8 +5,32 @@
  * Return 0 if every integer is between 1 and n^2 and all
  * n^2 integers are unique, otherwise return 1.
  */
-int check_group(int **elements, int n) {
+int check_group(int **elements, int n)
+{
     // TODO: replace this return statement with a real function body
+    int numbers[n * n];
+    int count = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            if (elements[i][j] < 1 || elements[i][j] > n * n)
+                return 1;
+            numbers[count] = elements[i][j];
+            count++;
+        }
+    }
+
+    for (int i = 0; i < n * n; ++i)
+    {
+        for (int j = 0; j < n * n; ++j)
+        {
+            if (i != j && numbers[i] == numbers[j])
+            {
+                return 1;
+            }
+        }
+    }
     return 0;
 }
 
@@ -16,8 +40,71 @@ int check_group(int **elements, int n) {
  * only use check_group to determine this by calling it on
  * each row, each column and each of the 9 inner 3x3 squares
  */
-int check_regular_sudoku(int **puzzle) {
+int check_regular_sudoku(int **puzzle)
+{
 
-    // TODO: replace this return statement with a real function body
+    // Check rows
+    int group1[3];
+    int group2[3];
+    int group3[3];
+    int *group[3] = {group1, group2, group3};
+
+    // Check Rows
+    for (int i = 0; i < 9; i++)
+    {
+        group1[0] = puzzle[i][0];
+        group1[1] = puzzle[i][1];
+        group1[2] = puzzle[i][2];
+
+        group2[0] = puzzle[i][3];
+        group2[1] = puzzle[i][4];
+        group2[2] = puzzle[i][5];
+
+        group3[0] = puzzle[i][6];
+        group3[1] = puzzle[i][7];
+        group3[2] = puzzle[i][8];
+        if (check_group(group, 3))
+            return 1;
+    }
+
+    // Check cols
+    for (int i = 0; i < 9; i++)
+    {
+        group1[0] = puzzle[0][i];
+        group1[1] = puzzle[1][i];
+        group1[2] = puzzle[2][i];
+
+        group2[0] = puzzle[3][i];
+        group2[1] = puzzle[4][i];
+        group2[2] = puzzle[5][i];
+
+        group3[0] = puzzle[6][i];
+        group3[1] = puzzle[7][i];
+        group3[2] = puzzle[8][i];
+        if (check_group(group, 3))
+            return 1;
+    }
+
+    // Check blocks
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            group1[0] = puzzle[0 + 3 * i][0 + 3 * j];
+            group1[1] = puzzle[0 + 3 * i][1 + 3 * j];
+            group1[2] = puzzle[0 + 3 * i][2 + 3 * j];
+
+            group2[0] = puzzle[1 + 3 * i][0 + 3 * j];
+            group2[1] = puzzle[1 + 3 * i][1 + 3 * j];
+            group2[2] = puzzle[1 + 3 * i][2 + 3 * j];
+
+            group3[0] = puzzle[2 + 3 * i][0 + 3 * j];
+            group3[1] = puzzle[2 + 3 * i][1 + 3 * j];
+            group3[2] = puzzle[2 + 3 * i][2 + 3 * j];
+            if (check_group(group, 3))
+                return 1;
+        }
+    }
+
     return 0;
 }

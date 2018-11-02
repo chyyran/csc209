c1dde0bae3fb99c1ddf4924f61319f3526ff8c03
Ronny Chan <ronny6993@gmail.com>
2018-11-02 13:15:55 -0400

    lab8

--- Lab8/checkpasswd.c
+++ Lab8/checkpasswd.c
@@ -12,20 +12,88 @@
 #define INVALID "Invalid password\n"
 #define NO_USER "No such user\n"
 
-int main(void) {
-  char user_id[MAXLINE];
-  char password[MAXLINE];
+int main(void)
+{
+    char user_id[MAXLINE];
+    char password[MAXLINE];
+    char buf[MAX_PASSWORD];
+    int pipefd[2];
+    pipe(pipefd);
 
-  if(fgets(user_id, MAXLINE, stdin) == NULL) {
-      perror("fgets");
-      exit(1);
-  }
-  if(fgets(password, MAXLINE, stdin) == NULL) {
-      perror("fgets");
-      exit(1);
-  }
-  
+    if (fgets(user_id, MAXLINE, stdin) == NULL)
+    {
+        perror("fgets");
+        exit(1);
+    }
+    if (fgets(password, MAXLINE, stdin) == NULL)
+    {
+        perror("fgets");
+        exit(1);
+    }
 
+    pid_t pid = fork();
 
-  return 0;
+    if (pid < 0)
+    {
+        perror("fork");
+        exit(1);
+    }
+
+    if (pid != 0)
+    {
+        close(pipefd[0]);
+        memset(buf, 0, MAX_PASSWORD);
+        memcpy(buf, user_id, MAX_PASSWORD);
+
+        write(pipefd[1], buf, MAX_PASSWORD);
+
+        memset(buf, 0, MAX_PASSWORD);
+        memcpy(buf, password, MAX_PASSWORD);
+
+        write(pipefd[1], buf, MAX_PASSWORD);
+    }
+
+    if (pid == 0)
+    {
+        close(pipefd[1]);
+        dup2(pipefd[0], STDIN_FILENO);
+        close(pipefd[0]);
+        int pipefd2[2];
+        pipe(pipefd2);
+
+        pid_t _pid = fork();
+
+        if (_pid < 0)
+        {
+            perror("fork");
+            exit(1);
+        }
+
+        if (_pid != 0)
+        {
+            int result;
+            waitpid(pid, &result, 0);
+            switch (WEXITSTATUS(result))
+            {
+            case 0:
+                printf(SUCCESS);
+                break;
+            case 1:
+                printf("ERROR\n");
+                break;
+            case 2:
+                printf(INVALID);
+                break;
+            case 3:
+                printf(NO_USER);
+                break;
+            }
+        }
+
+        if (_pid == 0) {
+            execl("./validate", "validate", NULL);
+        }
+    }
+
+    return 0;
 }

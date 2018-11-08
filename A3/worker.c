df191455685987927adaadd541c5bda8e4c6a9d4
Ronny Chan <ronny6993@gmail.com>
2018-11-08 03:13:03 -0500

    sketch out APIs for manipulating workers and the master array.

--- A3/worker.c
+++ A3/worker.c
@@ -7,15 +7,13 @@
 #include "freq_list.h"
 #include "worker.h"
 
+const FreqRecord record_sentinel = {0, ""};
+
 /* Complete this function for Task 1. Including fixing this comment.
 * Gets an array of FreqRecords for the given word. 
 */
 FreqRecord *get_word(char *word, Node *head, char **file_names)
 {
-
-    // instantiate sentinel
-    FreqRecord sentinel = {0, ""};
-
     // Search for the word in the record
     while (head)
     {
@@ -30,7 +28,7 @@ FreqRecord *get_word(char *word, Node *head, char **file_names)
     if (!head)
     {
         FreqRecord *returnRecord = malloc(sizeof(FreqRecord));
-        memcpy(returnRecord, &sentinel, sizeof(sentinel));
+        memcpy(returnRecord, &record_sentinel, sizeof(FreqRecord));
         return returnRecord;
     }
 
@@ -56,7 +54,7 @@ FreqRecord *get_word(char *word, Node *head, char **file_names)
         // allocate enough space for the next record.
         returnRecord = realloc(returnRecord, sizeof(FreqRecord) * (recordCount + 1));
     }
-    memcpy(&returnRecord[recordCount], &sentinel, sizeof(sentinel));
+    memcpy(&returnRecord[recordCount], &record_sentinel, sizeof(FreqRecord));
     return returnRecord;
 }
 
@@ -78,12 +76,12 @@ void print_freq_records(FreqRecord *frp)
 */
 void run_worker(char *dirname, int in, int out)
 {
-   
+
     char *listfile = malloc(sizeof(char) * (strlen(dirname) + strlen("/index") + 0x20));
     char *namefile = malloc(sizeof(char) * (strlen(dirname) + strlen("/filenames") + 0x20));
 
-    sprintf(listfile, "%s/%s\0", dirname, "index");
-    sprintf(namefile, "%s/%s\0", dirname, "filenames");
+    sprintf(listfile, "%s/%s", dirname, "index");
+    sprintf(namefile, "%s/%s", dirname, "filenames");
 
     Node *head = NULL;
     char **filenames = init_filenames();
@@ -94,14 +92,12 @@ void run_worker(char *dirname, int in, int out)
     char buf[MAXWORD];
     memset(buf, 0, MAXWORD);
 
-    // We may re-use the sentinel
-    const FreqRecord sentinel = {0, ""};
-
-    while ((readbytes = read(in, buf, MAXWORD)) <= MAXWORD && readbytes != 0)
+    while ((readbytes = read(in, buf, MAXWORD)) <= MAXWORD && readbytes != 0 && readbytes != EOF)
     {
         int i = 0;
         FreqRecord *records = get_word(buf, head, filenames);
-        while (records != NULL && records[i].freq != 0) {
+        while (records != NULL && records[i].freq != 0)
+        {
             write(out, &records[i], sizeof(FreqRecord));
             printf("\n%d: %s\n", records[i].freq, records[i].filename);
             i++;
@@ -110,7 +106,7 @@ void run_worker(char *dirname, int in, int out)
         memset(buf, 0, MAXWORD); // better safe than sorry, flush the buffer.
 
         // write the sentinel
-        write(out, &sentinel, sizeof(FreqRecord));
+        write(out, &record_sentinel, sizeof(FreqRecord));
         free(records);
     }
 
@@ -119,3 +115,143 @@ void run_worker(char *dirname, int in, int out)
     //todo: free init_filenames
     return;
 }
+
+// todo: remove pragmas
+#ifndef __GNUC__
+#pragma region master
+#endif
+
+typedef struct master_s
+{
+    FreqRecord records[MAXRECORDS];
+    int count;
+    int next;
+} master_s;
+
+MasterArray *instantiate_master_array()
+{
+    MasterArray *arr = malloc(sizeof(master_s));
+    arr->count = 0;
+
+    // next is incremented before insertion.
+    arr->next = -1;
+
+    // clear all freq records.
+    memset(arr->records, 0, sizeof(FreqRecord) * MAXRECORDS);
+    return arr;
+}
+
+void insert_record(MasterArray *arr, FreqRecord *frp)
+{
+    // do insertion here...
+    // full
+
+
+    if (arr->count < MAXRECORDS) {
+        arr->count++;
+        // do caculation of arr->next here, 
+        // for after sort.
+        // this should also ensure the array is sorted, shifting the array
+        // if necessary...
+        // use memmove.
+
+    } else {
+        // find the least, set that index to arr->next
+    }   
+
+    // note: this is a copy.
+    arr->records[arr->next] = *frp;
+}
+
+FreqRecord *get_record(MasterArray *arr, int i)
+{
+    return &arr->records[i];
+}
+
+#ifndef __GNUC__
+#pragma endregion
+#endif
+
+// todo: remove pragmas
+#ifndef __GNUC__
+#pragma region worker
+#endif
+
+typedef struct worker_s
+{
+    int fd_write;
+    int fd_read;
+
+    char path[128];
+    char sendbuf[32];
+} worker_s;
+
+Worker *worker_create(const char *path)
+{
+    if (strlen(path) >= 128)
+    {
+        perror("worker_create: dirname too long");
+    }
+
+    Worker *w = malloc(sizeof(worker_s));
+    int pipefd[2];
+
+    // should be safe from strlen check before.
+    memset(w->path, 0, 128);
+    strcpy(w->path, path);
+
+    pipe(pipefd);
+
+    w->fd_write = pipefd[0];
+    w->fd_read = pipefd[1];
+    return w;
+}
+
+
+void worker_close_write(Worker *w)
+{
+    if (w->fd_write == -1)
+        return;
+    close(w->fd_write);
+    w->fd_write = -1;
+}
+
+void worker_close_read(Worker *w)
+{
+    if (w->fd_read == -1)
+        return;
+    close(w->fd_read);
+    w->fd_read = -1;
+}
+
+ssize_t worker_send(Worker *w, const char *word) 
+{
+    if (w->fd_write == -1) return 0;
+    // prepare buffer for a safe send
+    // instead of allocating a new buffer on the stack, 
+    // have one reused for performance and safety reasons.
+    memset(w->sendbuf, 0, 32);
+    strncpy(w->sendbuf, word, 31);
+    w->sendbuf[31] = '\0';
+
+    return write(w->fd_write, w->sendbuf, 32);
+}
+
+ssize_t worker_recv(const Worker *w, FreqRecord *frp)
+{
+    if (w->fd_read == -1) return 0;
+    return read(w->fd_read, frp, sizeof(FreqRecord));
+}
+
+void worker_free(Worker *w)
+{
+    // close opened pipes
+    worker_close_read(w);
+    worker_close_write(w);
+
+    free(w);
+}
+
+#ifndef __GNUC__
+#pragma endregion
+#endif
--- A3/worker.h
+++ A3/worker.h
@@ -6,6 +6,8 @@
 
 #define MAXWORKERS 10
 
+#include <sys/select.h>
+
 // This data structure is used by the workers to prepare the output
 // to be sent to the master process.
 
@@ -17,5 +19,127 @@ typedef struct {
 FreqRecord *get_word(char *word, Node *head, char **file_names);
 void print_freq_records(FreqRecord *frp);
 void run_worker(char *dirname, int in, int out);
+
+
+// --- Master Array APIs
+/**
+ * Opaque type for MasterArray
+ */
+typedef struct master_s MasterArray;
+
+/**
+ * Instantiates a master array on the heap.
+ */
+MasterArray *instantiate_master_array();
+
+/**
+ * Inserts a new FreqRecord into the MasterArray following the given conditions
+ * 
+ * - After insertion, the MasterArray is sorted.
+ * - If the array is full it will replace the least frequent record.
+ */ 
+void insert_record(MasterArray *array, FreqRecord *frp);
+
+/**
+ * Retrieves a pointer to the record in the MasterArray 
+ */
+FreqRecord *get_record(MasterArray *array, int i);
+
+// --- Worker APIs
+
+/**
+ * Opaque type for Worker.
+ */
+typedef struct worker_s Worker;
+
+/**
+ * Creates a heap-allocated worker for the given directory.
+ * 
+ * Workers are not executed until called on by worker_run,
+ * and read/write pipes are automatically opened for them.
+ * 
+ * Workers own and manage their own resources, including the 
+ * opened pipes and a small write buffer used for writing data
+ * to the Worker. Such owned members should never be accessed 
+ * directly.
+ * 
+ * Instead, Workers should be manipulated only by the worker_* APIs
+ * in worker.h to prevent any resource leaks.
+ * 
+ * Remember to always free this worker after use with
+ * worker_free.
+ */
+Worker *worker_create(const char *dirname);
+
+/**
+ * Asynchronously begins the run loop for this worker. The worker will 
+ * have its pipes remained open for write and read from the calling 
+ * process/thread. 
+ * 
+ * Use the worker_close_* APIs to properly close the correct pipe on the 
+ * calling process.
+ * 
+ * Within the spawned process where the run loop executes,
+ * the worker will be closed for writes.
+ * 
+ * To stop this worker, send an EOF or close the underlying write pipe
+ * by calling worker_close_write.
+ */
+void worker_run(const Worker *w);
+
+/**
+ * Frees the worker, releasing all memory and file descriptors.
+ */
+void worker_free(Worker *w);
+
+/**
+ * Closes the worker for writes on this process.
+ * Once closed, the underlying pipe can never be reopened.
+ */
+void worker_close_write(Worker *w);
+
+/**
+ * Closes the worker for reads on this process.
+ * Once closed, the underlying pipe can never be reopened.
+ */
+void worker_close_read(Worker *w);
+
+/**
+ * Send a word to the given worker.
+ * 
+ * This method returns the result of the underlying
+ * write call that writes to the input pipe created for
+ * the given worker by worker_create: the number of bytes
+ * written to the pipe.
+ * 
+ * If the word written is greater than 31 characters, it 
+ * will be truncated. This method mutates the worker
+ * due to reusing an underlying buffer for the write.
+ * 
+ * If the pipe has been closed previously by 
+ * worker_close_write, this method returns 0.
+ * 
+ * To avoid confusion, this method is intentionally named
+ * worker_send instead of worker_write to indicate that
+ * it can only send words.
+ */
+ssize_t worker_send(Worker *w, const char *word);
+
+/**
+ * Waits and receives for a FreqRecord from this worker.
+ *
+ * This method returns the result of the underlying
+ * read call that reads from the output pipe created for
+ * the given worker by worker_create: the number of bytes
+ * read from the pipe
+ * 
+ * If the pipe has been closed previously by 
+ * worker_close_read this method returns 0.
+ * 
+ * To avoid confusion, this method is intentionally named
+ * worker_recv instead of worker_read to indicate that
+ * it can only send words.
+ */
+ssize_t worker_recv(const Worker *w, FreqRecord *record);
 
 #endif /* WORKER_H */
